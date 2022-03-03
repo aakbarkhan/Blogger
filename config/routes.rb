9b14065ea8ba3b447f15f7871681c25ceb38f3b5
aakbarkhan <aakbarkhan@gmail.com>
2022-03-03 12:41:31 +0530

    Add the users controller

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # root to: 'users#index'
+  # get '/users/', to: 'users#index' # path, to index usercontroller --> method
+  # get "/users/:id", to: "users#show"
+  resources :users , only: [:index, :show] 
+  resources :posts, only: [:index, :show]
+  
+
+end
