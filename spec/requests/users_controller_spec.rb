5cade70cd8ac29924308f8f48f0f3b2b1acf7bec
aakbarkhan <aakbarkhan@gmail.com>
2022-03-12 00:59:34 +0530

    Add the test case

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -3,28 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     before(:example) { get users_path }
-    it 'Successfuly get route' do
+
+    it 'should be a success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "Renders 'index' template for users" do
+    it "should render 'index' template" do
       expect(response).to render_template('index')
     end
-    it 'Checks if users/index html elements rendered' do
-      expect(response.body).to include('users index')
-    end
   end
+
   describe 'GET /show' do
     before(:example) { get user_path(1) }
-
-    it 'Response staturs correct' do
-      expect(response).to have_http_status(:success)
-    end
-    it "Renders 'show' template for users" do
-      expect(response).to render_template('show')
-    end
-    it 'Checks if response body includes correct placeholder text' do
-      expect(response.body).to include('This is user 1')
-    end
   end
 end
