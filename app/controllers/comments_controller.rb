0529b9c10bf1d87cdca57ede1c5317f611354862
aakbarkhan <aakbarkhan@gmail.com>
2022-03-11 12:42:59 +0530

    changes the comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,28 @@
 class CommentsController < ApplicationController
     def new
-      @comment =Comment.new
-    #   @user = User.find(params[:user_id])
-    #   @post = @user.posts.find(params[:post_id])
+      @comment = Comment.new
     end
   
     def create
-    #   @post = Post.find(params[:post_id])
-    # #   @post = Post.find(params[:post_id])
-    #   @user = User.find(params[:user_id])
-    @user = User.find(params[:user_id])
-    @posts = @user.posts
-
+      @post = Post.find(params[:post_id])
       @new_comment = @post.comments.new(
-        comment_params
-        # author_id: @user.id,
-        # post_id: @post.id
+        text: comment_params,
+        author_id: @post.id,
+        post_id: @post.id
       )
-    #   @new_comment.post_id = @post.id
+      @new_comment.post_id = @post.id
+      @new_comment.update_comments_counter
       if @new_comment.save
-        # Comment.comment_counter(@post.id)
-        Comment.count_comments(@post.id) 
-        redirect_to "/users/#{@post.author_id}/posts/#{@post.id}", notice: 'Posted Success!'
+        redirect_to "/users/#{@post.author_id}/posts/#{@post.id}", notice: 'Success!'
+        # redirect_to user_post_path(@post.id, Post.find(params[:post_id]))
       else
-        render :new, alert: 'Failed to Post the Comment'
+        render :new, alert: 'Failed to Create!'
       end
     end
-
+  
+    private
+  
     def comment_params
-        params.require(:comment).permit(:text)
+      params.require(:comment).permit(:text)[:text]
     end
-end
+  end
