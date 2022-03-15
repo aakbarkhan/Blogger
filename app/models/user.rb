e926c9be1a7dffbb56a200bec382fe292b3062b4
aakbarkhan <aakbarkhan@gmail.com>
2022-03-15 20:47:10 +0530

    Add the cofirmale in the user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :name, presence: true
   has_many :posts, foreign_key: 'author_id'
