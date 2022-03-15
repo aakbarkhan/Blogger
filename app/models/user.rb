bc1a8f44f85e277d742c86116c5a1cfd4a261aec
aakbarkhan <aakbarkhan@gmail.com>
2022-03-15 16:20:23 +0530

    generate the usermodel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :name, presence: true
   has_many :posts, foreign_key: 'author_id'
