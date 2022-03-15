61e66e25e0f9969138f6a27964f3055cf6339c9e
aakbarkhan <aakbarkhan@gmail.com>
2022-03-15 16:27:07 +0530

    Fix the linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # end
   protect_from_forgery with: :exception
   # before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
   def update_allowed_parameters
