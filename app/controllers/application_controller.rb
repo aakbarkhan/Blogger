33d1eed26a5047c13a6cd3856501d1755917b019
aakbarkhan <aakbarkhan@gmail.com>
2022-03-15 16:19:42 +0530

    add the controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
-  def self.current_user
-    User.first
+  # def self.current_user
+  #   User.first
+  # end
+  protect_from_forgery with: :exception
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :bio)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :surname, :email, :password, :current_password)
+    end
   end
 end
