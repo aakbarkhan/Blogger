f41613c64c2faed96a8219178b3e2002bb0c3d0a
aakbarkhan <aakbarkhan@gmail.com>
2022-03-15 16:22:52 +0530

    Add the route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # root to: 'users#index'
-  # get '/users/', to: 'users#index' # path, to index usercontroller --> method
-  # get "/users/:id", to: "users#show"
-  # resources :users, only: %i[index show]
-  # resources :posts, only: %i[index show]
-  
+  devise_for :users
   root to: 'users#index'
-  resources :users, only: %i[show index] do
-    resources :posts, only: %i[show index new create]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index create new show]
   end
 
   resources :posts do
@@ -21,6 +11,3 @@ Rails.application.routes.draw do
     resources :likes, only: %i[create]
   end
 end
-
-
-# root 'users#index' resources :users, only: [:index, :show] do resources :posts, only: [:index, :show] end 
